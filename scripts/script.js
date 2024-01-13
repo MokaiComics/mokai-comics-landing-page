4a892580c7e0b0ac7f6a6f32bf6ada6f0d8eaea4
Alexson Almeida <as3904527@gmail.com>
2024-01-13 15:01:59 -0300

    mobile version

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById('menu').classList.toggle('hidden');
+}
+
+function closeMenu() {
+    document.getElementById('menu').classList.add('hidden');
+}
---
+++ scripts/scrollReveal.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset: true});
+ScrollReveal({distance: '1rem'});
+
+ScrollReveal().reveal('.area1', {duration: 3000, origin: 'bottom'});
+ScrollReveal().reveal('.area2', {duration: 3000,  origin: 'bottom'});
+ScrollReveal().reveal('.area3', {duration: 3000,  origin: 'bottom'});
